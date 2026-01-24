5d46f5a2264b68ccd80cdfa288f2890530f496ed
TheodorPredescu <predescutheodor@gmail.com>
2026-01-24 18:48:01 +0200

    added vizualization for the changes in git

--- init.lua
+++ init.lua
@@ -168,8 +168,26 @@ require("lazy").setup({
     keys = {
         { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre", -- load when a buffer is opened
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = {hl = "GitGutterAdd", text = "│", numhl="GitGutterAddNr", linehl="GitGutterAddLn"},
+          change       = {hl = "GitGutterChange", text = "│", numhl="GitGutterChangeNr", linehl="GitGutterChangeLn"},
+          delete       = {hl = "GitGutterDelete", text = "_", numhl="GitGutterDeleteNr", linehl="GitGutterDeleteLn"},
+          topdelete    = {hl = "GitGutterDelete", text = "‾", numhl="GitGutterDeleteNr", linehl="GitGutterDeleteLn"},
+          changedelete = {hl = "GitGutterChange", text = "~", numhl="GitGutterChangeNr", linehl="GitGutterChangeLn"},
+        },
+        current_line_blame = false, -- shows git blame for current line
+        sign_priority = 6,
+        update_debounce = 100,
+      })
+    end
   }
-
 })
 
 vim.diagnostic.config({
