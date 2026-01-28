62b4594bf64c31a6bda93900ded72f4d7577f473
TheodorPredescu <predescutheodor@gmail.com>
2026-01-28 15:29:59 +0200

    Fixed bug on windows

--- init.lua
+++ init.lua
@@ -405,15 +405,15 @@ vim.keymap.set("n", "<leader>o", ":Oil<CR>", { desc = "Exit terminal mode" })
 vim.keymap.set("t", "<C-n>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<leader>t", toggle_terminal, { desc = "Toggle terminal" })
 vim.keymap.set("n", "<leader>ct", function()
-  local ft = vim.bo.filetype
-  if ft ~= "typescript" and ft ~= "typescriptreact" then
-    vim.notify("Not a TypeScript file", vim.log.levels.WARN)
-    return
-  end
+    local ft = vim.bo.filetype
+    if ft ~= "typescript" and ft ~= "typescriptreact" then
+        vim.notify("Not a TypeScript file", vim.log.levels.WARN)
+        return
+    end
 
-  vim.api.nvim_put({ "/**", " * ", " */" }, "l", true, true)
-  vim.cmd("normal! kA")
+    vim.cmd([[normal! i/**<CR> * <CR> */<Esc>kkA]])
 end, { desc = "Comment TypeScript" })
+
 -- Bugs the lazygit interface. It acts like a terminal (its probably one in the backgroud).
 -- vim.keymap.set("t", "<leader>t", function()
 --   vim.api.nvim_feedkeys(
